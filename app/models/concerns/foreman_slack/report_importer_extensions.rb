0cb3e779637381a88e534512bd1feddd66133c82
Shlomi Zadok <shlomi@ben-hanna.com>
2016-08-24 18:34:24 +0300

    Add needed param to notify_on_report_error_with_slack

--- app/models/concerns/foreman_slack/report_importer_extensions.rb
+++ app/models/concerns/foreman_slack/report_importer_extensions.rb
@@ -8,12 +8,12 @@ module ForemanSlack
 
     private
 
-    def notify_on_report_error_with_slack
+    def notify_on_report_error_with_slack(mail_error_state)
       if Setting[:notify_slack_puppet_error] && report.error?
         slack = ForemanSlack::SlackNotify.new
         slack.notify(_('%s is in error state. Please check logs') % name)
       end
-      notify_on_report_error_without_slack
+      notify_on_report_error_without_slack(mail_error_state)
     end
   end
 end
